33f589b829c8954607e030846c39be7f33dabfdb
fz3p <fzepparelli@protonmail.com>
2018-11-30 19:14:37 +0100

    change owner and groupe

--- xplanet/xplanet_cloud.sh
+++ xplanet/xplanet_cloud.sh
@@ -9,7 +9,7 @@ rm $tmp # supprimer l'ancien fichier temporaire
 wget -O $tmp http://xplanet.sourceforge.net/clouds/clouds_2048.jpg # télécharge l'image
 
 if [ -f $tmp ] ; then # si le fichier a bien été téléchargée...
-  mogrify -resize 2000x1000 $tmp # redimenssionne l'image téléchargée pour qu'elle est la même résolution que la map "jour"
+  mogrify -resize 2000x1000 $tmp # redimensionne l'image téléchargée pour qu'elle est la même résolution que la map "jour"
   mv $tmp $img # remplace l'ancienne image par la nouvelle
-  chown -R jarvis:www-data $pat && chmod -R 775 $pat # change les droits sur le fichier
+  chown -R www-data:www-data $pat && chmod -R 775 $pat # change les droits sur le fichier
 fi
