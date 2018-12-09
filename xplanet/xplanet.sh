45a839754c63c92c9ee107e63a1ba6f954ea1b55
fz3p <fzepparelli@protonmail.com>
2018-12-09 19:24:20 +0100

    add for init.d for debian 8

--- xplanet/xplanet.sh
+++ xplanet/xplanet.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
+#
+#### BEGIN INIT INFO
+# Provides:          xplanet
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Should-Start:      $portmap
+# Should-Stop:       $portmap
+# X-Start-Before:    nis
+# X-Stop-After:      nis
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# X-Interactive:     true
+# Short-Description: Example initscript
+# Description:       This file should be used to construct scripts to be
+#                    placed in /etc/init.d.
+### END INIT INFO
 
 # TERRE
 xplanet -conf /var/www/xplanet/xplanet/xplanet.conf -output /var/www/xplanet/xplanet/img/xplanet_earth.png -wait 120 -body earth -latitude 40 -longitude 10 -geometry 500x500 &
